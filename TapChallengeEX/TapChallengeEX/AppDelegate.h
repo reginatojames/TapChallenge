c01d2c64e8b2c90acc8e3df7882f207b1e8ad09b
Unknown <tsam021@tecnicosuperiorekennedy.it>
2017-01-13 15:51:47 +0100

    first

---
+++ TapChallengeEX/TapChallengeEX/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapChallengeEX
+//
+//  Created by Reginato James on 13/01/17.
+//  Copyright © 2017 Reginato James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TapChallengeEX/TapChallengeEX/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  TapChallengeEX
+//
+//  Created by Reginato James on 13/01/17.
+//  Copyright © 2017 Reginato James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (nonatomic, weak) IBOutlet UILabel * tapsCountLabel;
+
+-(IBAction)buttonPressed:(id)sender;
+
+@end
+
